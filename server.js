1362175f88548e73e49e173b8a7049032dbfa6e1
Ocampo <mariaocampo0513@gmail.com>
2021-02-02 17:07:56 -0800

    sample handlebars

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/signUp', (req, res) => {
+    res.render('signUp');
+});
+
+app.listen(3000, () => {
+    console.log('The web server has started on port 3000');
+});
