bb172510443911e10752ecfb2f578d53b55abf5e
Ocampo <mariaocampo0513@gmail.com>
2021-02-10 14:55:18 -0800

    updated navbar and fix a route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+var PORT = process.env.PORT || 8080;
 const exphbs = require('express-handlebars');
 const app = express();
+// var sassMiddleware = require("node-sass-middleware");
+// app.use(sassMiddleware({
+//     src: __dirname + "/sass",
+//     dest: '/public'
+//   }));
+
+app.use(express.static("public"));
+
 
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
@@ -10,18 +19,20 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
 
 
-app.get('/signUp', (req, res) => {
-    res.render('signUp');
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 app.get('/register', (req, res) => {
     res.render('./users/registration');
 });
 
-app.listen(3000, () => {
-    console.log('The web server has started on port 3000');
-});
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
